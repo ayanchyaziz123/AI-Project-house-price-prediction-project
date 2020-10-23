8d51307f8901433820c8e709c1e7da9ac6d27bb4
Izhaan Ahmed Chy <aaziz9642@gmail.com>
2020-10-23 17:47:29 +0600

    added database

--- main_project/house_price_prediction/home/views.py
+++ main_project/house_price_prediction/home/views.py
@@ -16,4 +16,8 @@ def contactUs(request):
 def aboutUs(request):
     return render(request, 'aboutUs.html') 
 def houses(request):
-    return render(request, 'houses.html')    
+    house = House.objects.all().order_by('-post_creatDate')[0:8]
+    context = {
+        'house':house
+    }
+    return render(request, 'houses.html', context)    
